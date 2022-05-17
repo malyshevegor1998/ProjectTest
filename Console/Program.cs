1080f87e25047014fa0ccceab03cdf510790e62c
unknown <malyshevegor1998@gmail.com>
2022-05-17 23:25:25 +0500

    Изменил файл main

--- Console/Program.cs
+++ Console/Program.cs
@@ -10,7 +10,7 @@ namespace Console
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Leha privet!");
+            System.Console.WriteLine("Egor privet!");
             System.Console.ReadLine();
         }
     }
